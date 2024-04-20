c4557f579541afa7cf1140c44f8b8b974591452a
HenriAguiar <aguiar.henri1@gmail.com>
2024-04-19 21:46:51 -0300

    estiliza produto

--- components/produto.js
+++ components/produto.js
@@ -1,23 +1,9 @@
-import React, { useState, useEffect } from 'react';
-import { FlatList, TextInput, View, SafeAreaView, StyleSheet } from 'react-native';
-import {
-    List,
-    Text,
-    IconButton,
-    Divider,
-    useTheme,
-    Modal,
-    Portal,
-    Button,
-    Avatar
-} from 'react-native-paper';
+import { View, Text, StyleSheet } from 'react-native';
+import { Button, Avatar } from 'react-native-paper';
 import { useAppContext } from './provider';
-import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
-export default function Produto({item}) {
-    console.log("bananaaaaaaaaaaaaaa")
-    console.log({item})
-    const { carrinho, adicionarProduto, removerProduto, } = useAppContext();
+export default function Produto({ item }) {
+    const { carrinho, adicionarProduto, removerProduto } = useAppContext();
     const getAvatarLetters = (title) => {
         const words = title.trim().split(' ');
         if (words.length > 1) {
@@ -27,29 +13,63 @@ export default function Produto({item}) {
     };
 
     return (
-        <View>
-        <Avatar.Text size={40} label={getAvatarLetters(item.Titulo)} />
-        <View style={{ padding: 10 }}>
-            <Text style={{ fontSize: 18, fontWeight: 'bold' }}>{item.Titulo}</Text>
-            <Text>{item.Descricao} - {item.Valor}</Text>
-            <Button
-                mode='contained'
-                labelStyle={{ color: 'white' }}
-                onPress={() => adicionarProduto(item.Id)}
-
-            >
-                +
-            </Button>
-            {carrinho.some(produto => produto.Id === item.Id) && (
+        <View style={styles.itemContainer}>
+            <Avatar.Text 
+                size={64} 
+                label={getAvatarLetters(item.Titulo)} 
+                style={styles.avatar}
+            />
+            <View style={styles.infoContainer}>
+                <Text style={styles.titulo}>{item.Titulo}</Text>
+                <Text style={styles.descricao}>{item.Descricao}</Text>
+                <Text style={styles.preco}>{item.Valor}</Text>
+            </View>
+            <View style={styles.botoesContainer}>
                 <Button
+                    icon="plus"
                     mode='contained'
-                    onPress={() => removerProduto(item.Id)}
-                    labelStyle={{ color: 'white' }}
-                >
-                    -
-                </Button>
-            )}
+                    onPress={() => adicionarProduto(item.Id)}
+                />
+                {carrinho.some(produto => produto.Id === item.Id) && (
+                    <Button
+                        icon="minus"
+                        mode='contained'
+                        onPress={() => removerProduto(item.Id)}
+                    />
+                )}
+            </View>
         </View>
-    </View>
-    )
+    );
 }
+
+const styles = StyleSheet.create({
+    itemContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        padding: 10,
+    },
+    avatar: {
+        marginRight: 10,
+    },
+    infoContainer: {
+        flex: 1,
+    },
+    titulo: {
+        fontSize: 18,
+        fontWeight: 'bold',
+    },
+    descricao: {
+        fontSize: 14,
+        color: 'gray',
+    },
+    preco: {
+        fontSize: 16,
+        fontWeight: 'bold',
+    },
+    botoesContainer: {
+        flexDirection: 'col',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        
+    },
+});
